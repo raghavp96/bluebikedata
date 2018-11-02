1c1ef6d3dc1a35244757c93aecb47b232cb80a6a
Raghavprasanna Rajagopalan <raghavp96@gmail.com>
2018-11-02 16:06:10 -0400

    Create DB Script

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # bluebikedata
 A database for blue bike data
+
+### Project Structure
++
+|
++----- db/
+	|
+	+----create\_bluebike\_db.sql
+
---
+++ db/create_bluebike_db.sql
@@ -0,0 +1,52 @@
+drop database if exists bluebike;
+
+create database bluebike;
+
+use bluebike;
+
+create table if not exists station (
+	station_id int primary key not null,
+    station_name varchar(50) not null,
+    latitude decimal(10) not null,
+    longitude decimal(10) not null,
+    short_name varchar(50) not null,
+    rental_methods enum('CREDITCARD', 'KEY') not null,
+    capacity int not null,
+    rental_id varchar(100) not null,
+    eightd_has_key_dispenser tinyint(2) not null,
+    has_kiosk tinyint(2) not null
+);
+
+create table if not exists trip (
+	trip_id int primary key not null,
+    bike_id int not null,
+    start_time datetime not null,
+    end_time datetime not null,
+    usertype enum('Customer', 'Subscriber') not null,
+    birthyear year not null,
+    gender tinyint not null,
+    start_station int not null,
+    stop_station int not null,
+    foreign key (start_station) references station(station_id),
+    foreign key (stop_station) references station(station_id)
+);
+
+create table if not exists station_status (
+	station_status_id int not null,
+    station_id int not null,
+    num_bikes_available int not null,
+    num_ebikes_available int not null,
+    num_bikes_disabled int not null,
+    num_docks_available int not null,
+    num_dock_disabled int not null,
+    is_installed tinyint not null,
+    is_rented tinyint not null,
+    is_returning tinyint not null,
+    last_reported datetime not null,
+    eightd_has_available_keys tinyint not null,
+    primary key (station_status_id, station_id),
+    foreign key (station_id) references station(station_id)    
+);
+
+-- Error Code: 1068. Multiple primary key defined
+
