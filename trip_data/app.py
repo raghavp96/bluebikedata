667c6a9ae6893251a899045d3a59a728942b338f
Sophie Wigmore <wigmore.s@husky.neu.edu>
2018-11-29 17:36:11 -0500

    add trips label to json in trip data

--- trip_data/app.py
+++ trip_data/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
-import csv, json
+import csv, json, requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def read_csv():
-    result = []
+    result = {"trips":[]}
     with open('files/201810-bluebikes-tripdata.csv', newline='') as csv_file:
         fieldnames = ("tripduration","starttime","stoptime","start_station_id","start_station_name",
         "start_station_latitude","start_station_longitude","end_station_id","end_station_name",
@@ -13,9 +14,9 @@ def read_csv():
         
         reader = csv.DictReader(csv_file, fieldnames)
         for row in reader:
-            result.append(row)
+            result.get("trips").append(row)
         # remove header row
-        result.pop(0)
+        result.get("trips").pop(0)
         return jsonify(result)
 
 
